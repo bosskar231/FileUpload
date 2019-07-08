7a7f705025ff5d9cfab56ad6aa5f42b5edf31c3c
baskar <bosskar231@gmail.com>
2019-07-08 17:21:08 +0530

    Project finished

--- src/main/java/com/auxo/resources/Producer.java
+++ src/main/java/com/auxo/resources/Producer.java
@@ -8,11 +8,12 @@ import java.util.Map;
 
 public class Producer {
 //    public static void main(String[] args) throws IOException {
-//        parseThis("hello");
+//        Producer producer=new Producer();
+//        producer.parseThis("E:\\Txt\\sample2.txt");
 //
 //    }
     //"E:\\Txt\\sample2.txt"
-    public String findByWord(String value,String path) throws IOException {
+    public String findByWord(String value,String path,int det) throws IOException {
         FileReader fr = new FileReader(new File(path));
         BufferedReader br = new BufferedReader(fr);
         String s;
@@ -24,12 +25,33 @@ public class Producer {
 
             if(s.indexOf(value)==0)
             {
-//                System.out.println(s);
+                if(det==1)
+                {
+                    return space(s);
+                }
+//                System.out.println();
+////                System.out.println(s);
                 return s;
             }
 
         }
         return  "hello";
+    }
+    public String space(String value)
+    {
+        String temp="";
+        for(int i=0;i<value.length();i++)
+        {
+            if(value.charAt(i)==' ')
+            {
+
+
+            }else
+            {
+                temp+=value.charAt(i);
+            }
+        }
+        return temp;
     }
     public JSONObject  parseThis(String path) throws IOException {
             JSONObject object=new JSONObject();
@@ -38,26 +60,26 @@ public class Producer {
             Producer producer=new Producer();
 
             String name,street,city,post,phone,addres,email;
-            name=producer.findByWord("Name:",path);
+            name=producer.findByWord("Name:",path,0);
             map.put("name",name.substring(6,name.length())   );
         Map address=new LinkedHashMap(3);
-        street=producer.findByWord("Street Name:",path);
+        street=producer.findByWord("Street Name:",path,0);
             address.put("street name",street.substring(12,street.length()));
-            city=producer.findByWord("City:",path);
+            city=producer.findByWord("City:",path,0);
         address.put("city",city.substring(6,city.length()));
-        post=producer.findByWord("Postal Code:",path);
-        address.put("postatl code",post.substring(12,post.length()));
+        post=producer.findByWord("Postal Code:",path,1);
+        address.put("postatl code",post.substring(post.length()-6,post.length()));
             map.put("address",address);
-            phone=producer.findByWord("Phone:",path);
-            map.put("phone",phone.substring(7,phone.length()));
-        if("hello"==producer.findByWord("Mail:",path))
+            phone=producer.findByWord("Phone:",path,1);
+            map.put("phone",phone.substring(phone.length()-10,phone.length()));
+        if("hello"==producer.findByWord("Mail:",path,0))
         {
-            email=producer.findByWord("Email Address:",path);
+            email=producer.findByWord("Email Address:",path,0);
             map.put("email", email.substring(15,email.length()));
 
         }else
         {
-            email=producer.findByWord("Mail:",path);
+            email=producer.findByWord("Mail:",path,0);
             map.put("email", email.substring(6,email.length()));
         }
 
